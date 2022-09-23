bbb33c95377ceee12a7343dbac4c0886891fdf52
robachicken <97412395+robachicken@users.noreply.github.com>
2022-09-23 13:23:55 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/docs/',
+  baseUrl: '/abcd/',
 onBrokenLinks: 'ignore',
 trailingSlash: true,
   favicon: 'img/favicon.ico',
